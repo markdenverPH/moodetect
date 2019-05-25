98d56994c41e19816efce80bbf1f9a6f10327225
markdenverPH <markdenverbabaran@gmail.com>
2019-05-25 16:13:35 +0800

    added designs; need to implement login and registration

--- app/src/main/java/com/example/moodetect2/Login.java
+++ app/src/main/java/com/example/moodetect2/Login.java
@@ -1,16 +1,70 @@
 package com.example.moodetect2;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btn_login, btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
 
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RegistrationForm registrationForm = new RegistrationForm();
+                registrationForm.show(getSupportFragmentManager(), "registration");
+            }
+        });
+    }
+
+    public void open_main_activity(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 
+    public static class RegistrationForm extends DialogFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.activity_registration_form, container, false);
+
+            if (getDialog() != null && getDialog().getWindow() != null) {
+                getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+//                getDialog().setCanceledOnTouchOutside(false);
+            }
+
+            return v;
+        }
+
+        @Override
+        public void onActivityCreated(Bundle arg0) {
+            super.onActivityCreated(arg0);
+//            getDialog().getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        }
+    }
 }
